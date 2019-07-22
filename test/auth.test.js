88daadcda5f8740338778f4021d59cad641ceb71
Dan Hogan <hogan.daniel@hotmail.com>
2019-07-22 16:39:26 -0700

    Use agent to sign in user before each test

--- test/auth.test.js
+++ test/auth.test.js
@@ -5,7 +5,6 @@ const request = require('supertest');
 const mongoose = require('mongoose');
 const connect = require('../lib/utils/connect');
 const User = require('../lib/models/User');
-const agent = require('superagent');
 
 describe('auth tests', () => {
 
@@ -17,15 +16,26 @@ describe('auth tests', () => {
     return mongoose.connection.dropDatabase();
   });
 
+  let user = null;
+  beforeEach(async() => {
+    user = await User.create({ userName: 'Noerda', profilePhotoUrl: 'www.blah.com/blah', password: 'abc123' });
+  });
+
+  const agent = request.agent(app);
+  beforeEach(() => {
+    return agent
+      .post('/api/v1/auth/signin')
+      .send({
+        userName: user.userName,
+        password: 'abc123'
+      });
+  });
+
 
   afterAll(() => {
     return mongoose.connection.close();
   });
 
-  let user = null;
-  beforeEach(async() => {
-    user = await User.create({ userName: 'Noerda', profilePhotoUrl: 'www.blah.com/blah', password: 'abc123' });
-  });
 
   it('can create a new user using /POST', () => {
     return request(app)
@@ -50,23 +60,14 @@ describe('auth tests', () => {
   });
 
   it('verifies user auth', ()  => {
-    const agent = request.agent(app);
     return agent
-      .post('/api/v1/auth/signin')
-      .send({
-        userName: user.userName,
-        password: 'abc123'
-      })
-      // eslint-disable-next-line no-unused-vars
-      .then(res => {
-        return agent
-          .get('/api/v1/auth/verify');
-      })
+      .get('/api/v1/auth/verify')
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
           userName: 'Noerda',
-          profilePhotoUrl: 'www.blah.com/blah'
+          profilePhotoUrl: 'www.blah.com/blah',
+          __v: 0
         });
       });
   });
