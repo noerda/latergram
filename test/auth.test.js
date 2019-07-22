53445f7ad4f1b4a5b71fcd2715daa3b7a029a97d
Dan Hogan <hogan.daniel@hotmail.com>
2019-07-22 15:28:47 -0700

    create and implement user sign in route/test

--- test/auth.test.js
+++ test/auth.test.js
@@ -4,6 +4,7 @@ const app = require('../lib/app');
 const request = require('supertest');
 const mongoose = require('mongoose');
 const connect = require('../lib/utils/connect');
+const User = require('../lib/models/User');
 
 describe('auth tests', () => {
 
@@ -15,8 +16,14 @@ describe('auth tests', () => {
     return mongoose.connection.dropDatabase();
   });
 
+
   afterAll(() => {
     return mongoose.connection.close();
+  });
+
+  let user = null;
+  beforeEach(async() => {
+    user = await User.create({ userName: 'Noerda', profilePhotoUrl: 'www.blach.com/blah', password: 'abc123' });
   });
 
   it('can create a new user using /POST', () => {
@@ -24,7 +31,17 @@ describe('auth tests', () => {
       .post('/api/v1/auth/signup')
       .send({ userName: 'Noerda', profilePhotoUrl: 'www.blach.com/blah', password: 'abc123' })
       .then(res => {
-        console.log(res.body)
+        expect(res.body).toEqual({
+          _id: expect.any(String), userName: 'Noerda', profilePhotoUrl: 'www.blach.com/blah', __v: 0
+        });
+      });
+  });
+
+  it('existing users can sign in using /POST', () => {
+    return request(app)
+      .post('/api/v1/auth/signin')
+      .send({ userName: user.userName, password: 'abc123' })
+      .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String), userName: 'Noerda', profilePhotoUrl: 'www.blach.com/blah', __v: 0
         });
