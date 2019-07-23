573ed5ab4d65edbc72224fcd777c486de942bc57
Dan Hogan <hogan.daniel@hotmail.com>
2019-07-23 15:22:20 -0700

    delete code that seed-data/data-helpers takes care of

--- test/auth.test.js
+++ test/auth.test.js
@@ -1,41 +1,8 @@
-require('dotenv').config();
-
 const app = require('../lib/app');
 const request = require('supertest');
-const mongoose = require('mongoose');
-const connect = require('../lib/utils/connect');
-const User = require('../lib/models/User');
+const { getAgent, getUsers } = require('./data-helpers');
 
 describe('auth tests', () => {
-
-  beforeAll(() => {
-    connect();
-  });
-
-  beforeEach(() => {
-    return mongoose.connection.dropDatabase();
-  });
-
-  let user = null;
-  beforeEach(async() => {
-    user = await User.create({ userName: 'Noerda', profilePhotoUrl: 'www.blah.com/blah', password: 'abc123' });
-  });
-
-  const agent = request.agent(app);
-  beforeEach(() => {
-    return agent
-      .post('/api/v1/auth/signin')
-      .send({
-        userName: user.userName,
-        password: 'abc123'
-      });
-  });
-
-
-  afterAll(() => {
-    return mongoose.connection.close();
-  });
-
 
   it('can create a new user using /POST', () => {
     return request(app)
@@ -49,24 +16,26 @@ describe('auth tests', () => {
   });
 
   it('existing users can sign in using /POST', () => {
+    const users = getUsers();
     return request(app)
       .post('/api/v1/auth/signin')
-      .send({ userName: user.userName, password: 'abc123' })
+      .send({ userName: users[0].userName, password: 'password' })
       .then(res => {
         expect(res.body).toEqual({
-          _id: expect.any(String), userName: 'Noerda', profilePhotoUrl: 'www.blah.com/blah', __v: 0
+          _id: expect.any(String), userName: users[0].userName, profilePhotoUrl: users[0].profilePhotoUrl, __v: 0
         });
       });
   });
 
-  it('verifies user auth', ()  => {
-    return agent
+  it('verifies user auth', async()  => {
+    const users = getUsers();
+    return getAgent()
       .get('/api/v1/auth/verify')
       .then(res => {
         expect(res.body).toEqual({
-          _id: expect.any(String),
-          userName: 'Noerda',
-          profilePhotoUrl: 'www.blah.com/blah',
+          _id: users[0]._id,
+          userName: users[0].userName,
+          profilePhotoUrl: users[0].profilePhotoUrl,
           __v: 0
         });
       });
