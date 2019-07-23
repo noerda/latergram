9bd7d40ecf49cf8aab3020597e9ea546b4d2592e
Dan Hogan <hogan.daniel@hotmail.com>
2019-07-23 16:17:00 -0700

    Create test for delete comment by ID

--- test/comments-router.test.js
+++ test/comments-router.test.js
@@ -1,4 +1,5 @@
-const { getAgent, getComments, getPosts, getUsers } = require('./data-helpers');
+const Comment = require('../lib/models/Comment');
+const { getAgent, getPosts, getUsers } = require('./data-helpers');
 
 describe('app routes', () => {
   it('can post a comment', () => {
@@ -16,6 +17,17 @@ describe('app routes', () => {
           __v: 0
         });
       });
+  });
 
+  it('deletes a comment by id', async() => {
+    const users = getUsers();
+    const posts = getPosts();
+    const comment = await Comment.create({ commentBy: users[0]._id, post: posts[0]._id, comment: 'Life is hard and then you die.' });
+    const commentJSON = JSON.parse(JSON.stringify(comment));
+    return getAgent()
+      .delete(`/api/v1/comments/${comment._id}`)
+      .then(res => {
+        expect(res.body).toEqual(commentJSON);
+      });
   });
 });
